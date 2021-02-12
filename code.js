5c4a5708398dff06d41d197d091b2d35a942f928
yoshisean <65581228+yoshisean@users.noreply.github.com>
2021-02-12 00:50:56 -0600

    Update code.js

--- code.js
+++ code.js
@@ -3,6 +3,7 @@ let name, text, sendtime, created_at;
 let signedin=false;
 let prevdate="";
 
+
 const firebaseConfig = {
     apiKey: "AIzaSyCnTZQPPcBytSUDtRKp4M6XDGPrwl9UFMA",
     authDomain: "chatapp-4d072.firebaseapp.com",
@@ -14,16 +15,15 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
+
 const w = window.innerWidth;
 const h = window.innerHeight;
 const tbposy=document.getElementById("chattext").offsetTop;
 const btnpos=h-tbposy-20;
 document.getElementById("sendbtn").style.bottom = btnpos;
-
 function scroll(){
     document.getElementById('chatbox').scrollTop = (document.getElementById('chatbox').scrollHeight - document.getElementById('chatbox').offsetHeight)
 }
-
 
 window.addEventListener('keydown',enterclicked );
 function enterclicked(e){
@@ -35,11 +35,14 @@ function enterclicked(e){
 }
 
 
+
 function authsend(username){
     signedin=true
     name=username
 }
-
+function getUname(){
+    return name
+}
 function send() {
     if (signedin ) {
         let message = document.getElementById("chattext").value;
@@ -62,7 +65,8 @@ function send() {
             firebase.database().ref("chats/" + Date.now()).set({
                 name: name,
                 created_at: sendtime,
-                text: text
+                text: text,
+                image:"false",
             })
             document.getElementById("chattext").value = ""
         }
@@ -95,5 +99,32 @@ function updatechatwwindow(name, time, text){
 
     chatbox.append(name_Element,text_Element,time_Element)
 
-    // chatbox.innerText += name + " at " + time + " : " + text + "\n"
 }
+
+async function addimage(name, url, time) {
+    let chatbox = document.getElementById("chatbox")
+
+    let date = time.substring(0, time.indexOf("@"))
+    let t2 = time.substring(time.indexOf("@")+1,time.length)
+    if(date!=prevdate){
+        let date_Element = document.createElement("em");
+        date_Element.innerText = date+"\n";
+        date_Element.className="date"
+        chatbox.append(date_Element)
+        prevdate=date;
+    }
+
+    let name_Element = document.createElement("em");
+    name_Element.innerText = name + ": ";
+    name_Element.className = "name"
+
+    let image_Element = document.createElement("IMG");
+    image_Element.src=url
+
+    let time_Element = document.createElement("h1");
+    time_Element.innerText = t2+"\n";
+    time_Element.className="time"
+
+    chatbox.append(name_Element,image_Element, time_Element)
+}
+
